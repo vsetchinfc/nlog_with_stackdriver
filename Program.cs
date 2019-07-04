2070aa02ae7f470521ed7d08adfa91c89a0724de
Vladislav Setchin <vlad@setchinfc.com.au>
2019-07-05 09:26:25 +1000

    Initial project checkin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nlog_with_stackdriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
